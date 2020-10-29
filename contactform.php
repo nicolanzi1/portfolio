366305e33e53b1ea13cc936a403592bff4f42dac
nicolanzi1 <nicolanzi@gmail.com>
2020-10-29 15:01:31 +0000

    Testing Contact Form's functionallity in the server

--- contactform.php
+++ contactform.php
@@ -1,32 +1,17 @@
 <?php
-$errors = '';
-$myemail = 'nicolanzi@gmail.com';
-if(empty($_POST['name']) ||
-   empty($_POST['email']) ||
-   empty($_POST['message']))
-{
-    $errors = "\n Error: all fields are required";
-}
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $POST['message'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-if(empty($errors))
-{
-$to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-"Here are the details:\n Name: $name \n".
-"Email: $email_address\n Message: \n $message";
-$headers = "From: $myemail\n";
-$headers .= "Reply-To: $email_address";
-mail($to, $email_subject, $email_body, $headers);
-header('Location: thanks.html');
-}
+    $from = 'nicolanzi@gmail.com';
+    $subject = "New form submission";
+    $body = "Name: $name.\n".
+                "Email: $email.\n".
+                    "Message: $message.\n";
+
+    $to = "nicolanzi@gmail.com";
+    $headers = "From: $from \r\n";
+    $headers .= "Reply to: $email \r\n";
+    mail($to,$subject,$body,$headers);
+    header("Location: thanks.html");
 ?>
