b506384f0bd8f57381244bc0cbb52081d6f31e42
nicolanzi1 <nicolanzi@gmail.com>
2020-10-29 22:13:43 +0000

    Added PHPMailer folder and modified the rest of the files

--- contactform.php
+++ contactform.php
@@ -1,49 +1,36 @@
 <?php
-    $result="";
-
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
     use PHPMailer\PHPMailer\PHPMailer;
 
-    $email_user = getenv['EMAIL_USER'];
-    $email_password = getenv['EMAIL_PASSWORD'];
-
-    if(isset($_POST['submit'])){
+    if (isset($_POST['name']) && isset($_POST['email'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $body = $_POST['body'];
 
-        require_once "phpmailer/src/PHPMailer.php";
-        require_once "phpmailer/src/SMTP.php";
-        require_once "phpmailer/src/Exception.php";
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
 
-        $mail = new PHPMailer;
+        $mail = new PHPMailer();
 
+        //SMTP Settings
         $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
+        $mail->Host = "smtp.gmail.com";
         $mail->SMTPAuth = true;
-        $mail->Username = $email_user;
-        $mail->Password = $email_password;
-        $mail->Port = 465;
-        $mail->SMTPSecure = 'ssl';
+        $mail->Username = 'nicolanzi@gmail.com';
+        $mail->Password = "";
+        $mail->Port = 465; //587
+        $mail->SMTPSecure = 'ssl'; //tls
 
+        //Email Settings
         $mail->isHTML(true);
         $mail->setFrom($email, $name);
-        $mail->addAddress('contact@nicolanz.io');
-        $mail->Body = $body;
+        $mail->addAddress("nicolanzi@gmail.com");
+        $email->Body = $body;
 
-        if($mail->send()){
-            $status = "success";
-            $response = "Your message has been sent. We'll be in touch...";
-        }
+        if ($mail->send())
+            $response =  "Your message was sent!";
         else
-        {
-            $status = "failed";
-            $response = "Something went wrong: <br>".$mail->ErrorInfo;
-        }
-
-        exit(json_decode(array("status" => $status, "response" => $response)));
+            $response =  "Something went wrong: <br><br>" . $mail->ErrorInfo; 
+        exit(json_encode(array("response" => $response)));
     }
-
 ?>
