247b87eae42c1d4e2f8cc652985c17fb12d1d4fa
nicolanzi1 <nicolanzi@gmail.com>
2020-11-09 17:30:17 +0000

    Added Google Analytics at the top of my page to measure visits among other things

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-51ME99RC8D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-51ME99RC8D');
+</script>
+
 <!DOCTYPE HTML>
 <!--
 	Dimension by HTML5 UP
